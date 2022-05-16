a55282718b562fa4a7a6ac3cee91caeb5a6fea0f
Matej Bredik <xbredik@stuba.sk>
2022-05-16 19:34:43 +0200

    Html Form

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+
+
 ?>
 
 <!doctype html>
@@ -8,9 +11,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Matejs title</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Finálne zadanie</title>
 </head>
 <body>
 <h1>Final</h1>
+<form action="index.php">
+    <label for="name">
+        <input type="text" name="name" id="name">
+    </label>
+    <label for="codeArea">
+        <textarea name="codeArea" id="codeArea" cols="30" rows="10"></textarea>
+    </label>
+    <input type="submit" name="submit" id="submit" value="Odoslať">
+</form>
 </body>
 </html>
